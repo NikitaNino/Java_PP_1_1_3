dde5f3b6832db2662e39831850add075b7b5f699
NikitaNino <nikita-vip1904@yandex.ru>
2022-08-01 13:19:49 +0300

    Adding a connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String NAMEUSER = "Admin";
+    public static final String PASSWORD = "1234";
+    public static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    public static Statement statement;
+    public static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, NAMEUSER, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.getStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
