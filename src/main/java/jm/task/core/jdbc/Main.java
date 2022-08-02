f4e2a93e2479f7cc6352f4191584f378fbd2c959
NikitaNino <nikita-vip1904@yandex.ru>
2022-08-02 12:21:02 +0300

    Correcting errors

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,19 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
-        userDaoJDBC.saveUser("Максим", "Максович", (byte) 23);
-        userDaoJDBC.saveUser("Антон", "Внтонович", (byte) 32);
-        userDaoJDBC.saveUser("Лев", "Левович", (byte) 25);
-        userDaoJDBC.saveUser("Юрий", "Юрьевич", (byte) 63);
-        userDaoJDBC.removeUserById(2);
-        userDaoJDBC.getAllUsers().forEach(x-> System.out.println(x.toString()));
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+        userDao.saveUser("Максим", "Максович", (byte) 23);
+        userDao.saveUser("Антон", "Викторович", (byte) 32);
+        userDao.saveUser("Лев", "Львович", (byte) 25);
+        userDao.saveUser("Юрий", "Юрьевич", (byte) 63);
+        userDao.removeUserById(2);
+        userDao.getAllUsers().forEach(x-> System.out.println(x.toString()));
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
         // реализуйте алгоритм здесь
     }
 }
