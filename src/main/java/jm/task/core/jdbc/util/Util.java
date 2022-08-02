d61f68668eb24941a26ad5d379fe98d7d55ebae7
NikitaNino <nikita-vip1904@yandex.ru>
2022-08-02 19:12:44 +0300

    changing the connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Util {
-    private static final String USERNAME = "Admin";
+    private static final String USERNAME = "root";
     private static final String PASSWORD = "1234";
-    private static final String URL = "jdbc:mysql://localhost:3306/name_db";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/name_db";
 
 
     public static Connection getConnection() {
