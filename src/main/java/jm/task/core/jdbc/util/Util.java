fe358f1bd1138327741bcd85f6c794de3dfe7629
NikitaNino <nikita-vip1904@yandex.ru>
2022-08-06 20:29:26 +0300

    Additions to Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -13,15 +13,15 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "1234";
+    private static final String USER = "root";
+    private static final String PASS = "1234";
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/name_db";
 
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection = DriverManager.getConnection(URL, USER, PASS);
         } catch (SQLException e) {
             System.err.println("Подключение не было выполнено");
         }
@@ -37,8 +37,8 @@ public class Util {
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://127.0.0.1:3306/test_db");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "1234");
+                settings.put(Environment.USER, USER);
+                settings.put(Environment.PASS, PASS);
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
